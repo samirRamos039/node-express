cf7d3ea8556ef2584fe047b70e8836bd1a213912
rzagza039@gmail.com <rzagza039@gmail.com>
2023-04-16 17:54:19 -0500

    modificando database

--- src/dataBase/database.js
+++ src/dataBase/database.js
@@ -5,3 +5,13 @@ export const sequelize = new Sequelize('nodeexpress','postgres','1234',{
     host: 'localhost',
     dialect: 'postgres'
 })
+
+import React, { memo } from 'react'
+
+const database = memo(() => {
+  return (
+    <div>database</div>
+  )
+})
+
+export default database
